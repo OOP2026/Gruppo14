9eb38dde175e2754e30f678f7698a141c26ccea9
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-06-12 16:45:02 +0200

    Update Lezione.java

--- src/main/java/model/Lezione.java
+++ src/main/java/model/Lezione.java
@@ -1,27 +1,29 @@
 package model;
 
-
-import java.util.Date;
-
 public class Lezione {
-	String insegnamento;
-	String giornoSettimana;
-	String oraInizio;
-	String oraFine;
-	String aula;
-	Docente docAssegnato;
+    private String insegnamento;
+    private String aula;
+    private String giornoSettimana;
+    private String oraInizio;
+    private String oraFine;
 
-	public Lezione(String insegnamento,String giornoSettimana,String oraInizio,String oraFine,String aula,Docente docAssegnato) {
-		this.insegnamento=insegnamento;
-		this.giornoSettimana=giornoSettimana;
-		this.oraInizio=oraInizio;
-		this.oraFine=oraFine;
-		this.aula=aula;
-		this.docAssegnato=docAssegnato;
-	}
-	@Override
-	public String toString() {
-		return insegnamento +" "+ giornoSettimana+" (" + oraInizio + "-" + oraFine + ", " + aula + ")";
-	}
+    public Lezione(String insegnamento, String aula, String giornoSettimana, String oraInizio, String oraFine) {
+        this.insegnamento = insegnamento;
+        this.aula = aula;
+        this.giornoSettimana = giornoSettimana;
+        this.oraInizio = oraInizio;
+        this.oraFine = oraFine;
+    }
+
+    // Getter e Setter
+    public String getInsegnamento() { return insegnamento; }
+    public void setInsegnamento(String insegnamento) { this.insegnamento = insegnamento; }
+    public String getAula() { return aula; }
+    public void setAula(String aula) { this.aula = aula; }
+    public String getGiornoSettimana() { return giornoSettimana; }
+    public void setGiornoSettimana(String giornoSettimana) { this.giornoSettimana = giornoSettimana; }
+    public String getOraInizio() { return oraInizio; }
+    public void setOraInizio(String oraInizio) { this.oraInizio = oraInizio; }
+    public String getOraFine() { return oraFine; }
+    public void setOraFine(String oraFine) { this.oraFine = oraFine; }
 }
-
