69c59f0e26046e31b30bd9faf35a6970ee206ad2
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-06-12 16:42:52 +0200

    Update Insegnamento.java

--- src/main/java/model/Insegnamento.java
+++ src/main/java/model/Insegnamento.java
@@ -1,19 +1,30 @@
-package Progetto;
+package model;
 
 public class Insegnamento {
-	String nomeInsegnamento;
-	int numeroCFU;
-	int annoCorso;
-	String docenteTitolare;
-	boolean attivo;
-	public Insegnamento(String nomeInsegnamento,int numeroCFU,int annoCorso,String docenteTitolare,boolean attivo) {
-		this.nomeInsegnamento=nomeInsegnamento;
-		this.numeroCFU=numeroCFU;
-		this.annoCorso=annoCorso;
-		this.docenteTitolare=docenteTitolare;
-		this.attivo=attivo;
-	}
-	public boolean isAttivo() {
-		return this.attivo;
-	};
+    private String nomeInsegnamento;
+    private int numeroCFU;
+    private int annoCorso;
+    private String docenteTitolare;
+
+    public Insegnamento(String nomeInsegnamento, int numeroCFU, int annoCorso, String docenteTitolare) {
+        this.nomeInsegnamento = nomeInsegnamento;
+        this.numeroCFU = numeroCFU;
+        this.annoCorso = annoCorso;
+        this.docenteTitolare = docenteTitolare;
+    }
+
+    public boolean isAttivo() {
+        // Logica per verificare se l'insegnamento è attivo
+        return true;
+    }
+
+    // Getter e Setter
+    public String getNomeInsegnamento() { return nomeInsegnamento; }
+    public void setNomeInsegnamento(String nomeInsegnamento) { this.nomeInsegnamento = nomeInsegnamento; }
+    public int getNumeroCFU() { return numeroCFU; }
+    public void setNumeroCFU(int numeroCFU) { this.numeroCFU = numeroCFU; }
+    public int getAnnoCorso() { return annoCorso; }
+    public void setAnnoCorso(int annoCorso) { this.annoCorso = annoCorso; }
+    public String getDocenteTitolare() { return docenteTitolare; }
+    public void setDocenteTitolare(String docenteTitolare) { this.docenteTitolare = docenteTitolare; }
 }
