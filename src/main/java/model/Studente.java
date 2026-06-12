f742791b744b10ee4788d8edfc017678da68b3fd
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-06-12 16:42:01 +0200

    Update Studente.java

--- src/main/java/model/Studente.java
+++ src/main/java/model/Studente.java
@@ -1,30 +1,22 @@
 package model;
 
 public class Studente extends Utente {
-    private final String  Matricola;
-    int annoDiCorso;
-    String facolta;
+    private String matricola;
+    private int annoDiCorso;
 
-
-    public Studente (String login, String password ,String Matricola, String email, String nome, String cognome,int annoDiCorso,String facolta){
-        //Chiamo il costruttore di utente
-        super(login,password,email,nome,cognome);
-        this.Matricola=Matricola;
-        this.annoDiCorso=annoDiCorso;
-        this.facolta=facolta;
-    }
-    protected String getMatricola() {
-        return Matricola;
+    public Studente(String nome, String cognome, String email, String password, String login, String matricola, int annoDiCorso) {
+        super(nome, cognome, email, password, login, Ruolo.STUDENTE);
+        this.matricola = matricola;
+        this.annoDiCorso = annoDiCorso;
     }
 
-    public Orario visualizzaOrario(Orario attuale,int annoDiCorso,String facolta){
-        if (attuale.getAnno() == annoDiCorso && attuale.getCorso().equals(facolta)) {
-            return attuale; // L'orario è già filtrato per corso e anno
-        } else {
-            System.out.println("Nessun orario disponibile per il corso '"
-                    + facolta + "' anno " + annoDiCorso);
-            return new Orario(); // Orario vuoto
-        }
+    public void visualizzaOrario() {
+        // Logica per mostrare l'orario allo studente
     }
 
+    // Getter e Setter
+    public String getMatricola() { return matricola; }
+    public void setMatricola(String matricola) { this.matricola = matricola; }
+    public int getAnnoDiCorso() { return annoDiCorso; }
+    public void setAnnoDiCorso(int annoDiCorso) { this.annoDiCorso = annoDiCorso; }
 }
