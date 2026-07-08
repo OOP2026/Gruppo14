ae88c09cb5a57125dd1013a28696890a78c3e7b8
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-07-08 19:53:52 +0200

    Create Aula.java

---
+++ src/main/java/model/Aula.java
@@ -0,0 +1,11 @@
+package Progetto;
+
+public class Aula {
+	String nomeAula;
+    public Aula(String nomeAula) {
+		this.nomeAula=nomeAula;
+	}
+	public String getNomeAula() {
+		return nomeAula;
+	}
+}
