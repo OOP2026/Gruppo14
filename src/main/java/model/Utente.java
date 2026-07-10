3731c3ff7aba6d78885a104b31c87b14918f9015
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-07-10 19:15:03 +0200

    Update Utente.java

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -5,6 +5,7 @@ public String nome;
 public String cognome;
 public String email;
 protected String password;
+public String login;
 public Utente(String nome,String cognome,String email,String password) {
 	this.nome=nome;
 	this.cognome=cognome;
