d8bcb84d7cd37ae2f6e9367323cdf34b27b0e196
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-07-17 09:27:10 +0200

    Update Utente.java

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -5,12 +5,13 @@ public String nome;
 public String cognome;
 public String email;
 protected String password;
-public String login;
-public Utente(String nome,String cognome,String email,String password) {
+public String username;
+public Utente(String nome,String cognome,String email,String password,String username) {
 	this.nome=nome;
 	this.cognome=cognome;
 	this.email=email;
 	this.password=password;
+	this.username=username;
 }
 public boolean toLogin(String email,String password) {
 	if (this.email.equals(email) && this.password.equals(password)) {
