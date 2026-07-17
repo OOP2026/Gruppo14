64688dabc22204d74acf9782f678bd1ae3589768
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-07-17 15:41:15 +0200

    Update Docente.java

--- src/main/java/model/Docente.java
+++ src/main/java/model/Docente.java
@@ -1,10 +1,15 @@
 package Progetto;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Docente extends Utente {
-	Insegnamento insegnamento;
-	public Docente(String nome,String cognome,String email,String password,Insegnamento insegnamento) {
-		super(nome,cognome,email,password);
-		this.insegnamento=insegnamento;
+	public List<Insegnamento> insegnamento;
+	ResponsabileOrari responsabile;
+	public Docente(String nome,String cognome,String email,String password,String username,ArrayList<Insegnamento>insegnamento,ResponsabileOrari responsabile) {
+		super(nome,cognome,email,password,username);
+		this.insegnamento=new ArrayList<>();
+		this.responsabile=responsabile;
 	}
-
+public String getCognome() {
+	return cognome;
+}
 }
