67c737f19dc5ef894c883b4d06234c8867a8be7c
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-07-17 16:13:20 +0200

    Update Aula.java

--- src/main/java/model/Aula.java
+++ src/main/java/model/Aula.java
@@ -1,11 +1,22 @@
 package Progetto;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Aula {
 	String nomeAula;
+	List<Lezione>lezioniOspitate;
     public Aula(String nomeAula) {
 		this.nomeAula=nomeAula;
+		this.lezioniOspitate=new ArrayList<>();
 	}
 	public String getNomeAula() {
 		return nomeAula;
 	}
+	public void aggiungiLezione(Lezione lezione) {
+        if (lezione != null) {
+            this.lezioniOspitate.add(lezione);
+        }
+	}
+	public List<Lezione> getLezioniOspitate() {
+        return lezioniOspitate;
+    }
 }
