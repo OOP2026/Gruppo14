5e097ffbf8f39c13b16487bef42d1c466b798c89
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-07-17 10:03:25 +0200

    Update Studente.java

--- src/main/java/model/Studente.java
+++ src/main/java/model/Studente.java
@@ -2,11 +2,20 @@ package Progetto;
 
 public class Studente extends Utente {
 	String matricola;
-	public Studente(String nome,String cognome,String email,String password,String matricola) {
-		super(nome,cognome,email,password);
+	public Studente(String nome,String cognome,String email,String password,String username,String matricola) {
+		super(nome,cognome,email,password,username);
 		this.matricola=matricola;
 		}
-	void aggiungiMatricola(String matricola) {
-		this.matricola=matricola;
+	public void aggiungiMatricola(String matricola) {
+		if(matricola!=null && !matricola.trim().isEmpty()) {
+			this.matricola=matricola;
+			System.out.println("Matricola" + matricola + "aggiunta con successo allo studente");
+		}
+		else {
+			System.out.println("Matricola non valida!");
+		}
+	}
+	public String getMatricola() {
+		return matricola;
 	}
 }
