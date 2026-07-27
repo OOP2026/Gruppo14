8c17fbe2f5f767361c5aaf4760d17b1bdfed0142
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-07-27 15:53:25 +0200

    Update Docente.java

--- src/main/java/model/Docente.java
+++ src/main/java/model/Docente.java
@@ -1,14 +1,40 @@
 package Progetto;
 import java.util.List;
 import java.util.ArrayList;
+/**
+ * Rappresenta un <b>Docente</b> all'interno dell'ateneo
+ */
 public class Docente extends Utente {
-	public List<Insegnamento> insegnamento;
+	/**
+	 * un docente può sostenere più insegnamenti
+	 */
+	public ArrayList<Insegnamento> insegnamento;
 	ResponsabileOrari responsabile;
-	public Docente(String nome,String cognome,String email,String password,String username,ArrayList<Insegnamento>insegnamento,ResponsabileOrari responsabile) {
+	/**
+	 * nel corso della giornata, un docente può tenere più lezioni
+	 */
+	public ArrayList<Lezione> lezioni;
+	/**
+	 * Costruttore della classe Docente:
+	 * @param nome
+	 * @param cognome
+	 * @param email
+	 * @param password
+	 * @param username
+	 * @param insegnamento
+	 * @param responsabile
+	 * @param lezioni
+	 */
+	public Docente(String nome,String cognome,String email,String password,String username,ArrayList<Insegnamento>insegnamento,ResponsabileOrari responsabile,ArrayList<Lezione> lezioni) {
 		super(nome,cognome,email,password,username);
 		this.insegnamento=new ArrayList<>();
 		this.responsabile=responsabile;
+		this.lezioni=new ArrayList<>();
 	}
+	/**
+	 * @return cognome
+	 *<br>Restituisce il cognome del docente</br>
+	 */
 public String getCognome() {
 	return cognome;
 }
