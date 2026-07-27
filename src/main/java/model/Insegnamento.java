7c1a9be9e86479112254f8591c1c0074f0ab78fb
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-07-27 17:03:25 +0200

    Update Insegnamento.java

--- src/main/java/model/Insegnamento.java
+++ src/main/java/model/Insegnamento.java
@@ -6,27 +6,54 @@ public String nome;
 public int numeroCFU;
 public AnnoCorso annoCorso;
 public Docente docenteTitolare;
-public List<Lezione> lezioni;
-public Insegnamento(String nome,int numeroCFU,AnnoCorso annoCorso,Docente docenteTitolare) {
+public List<Orario> orari; 
+/**
+ * Costruttore della classe Insegnamento:
+ * @param nome
+ * @param numeroCFU
+ * @param annoCorso
+ * @param docenteTitolare
+ * @param orari
+ */
+public Insegnamento(String nome,int numeroCFU,AnnoCorso annoCorso,Docente docenteTitolare,List<Orario> orari) {
 	this.nome=nome;
 	this.numeroCFU=numeroCFU;
 	this.annoCorso=annoCorso;
 	this.docenteTitolare=docenteTitolare;
-	this.lezioni=new ArrayList<>();
 }
+/**
+ * Restituisce il nome dell'insegnamento
+ * @return nome
+ */
 public String getNome() {
 	return nome;
 }
+/**
+ * Restituisce il numero di CFU di un determinato insegnamento
+ * @return numeroCFU
+ */
 public int getNumeroCFU() {
 	return numeroCFU;
 }
+/**
+ * Restituisce l'anno di corso (I,II,III) di un insegnamento
+ * @return annoCorso
+ */
 public AnnoCorso getAnnoCorso() {
 	return annoCorso;
 }
+/**
+ * Restituisce il docente dell'insegnamento
+ * @return docenteTitolare
+ */
 public Docente getDocenteTitolare() {
 	return docenteTitolare;
 }
-public List<Lezione> getLezioni() {
-	return lezioni;
+/**
+ * Restituisce l'insieme di orari a cui un insegnamento può appartenere
+ * @return orari
+ */
+public List<Orario> getOrari() {
+	return this.orari;
 }
 }
