6b76c97f2135a226fb7890a63cce854e72abd817
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-08-01 11:33:31 +0200

    c

--- src/main/java/model/AnnoCorso.java
+++
@@ -1,9 +0,0 @@
-package Progetto;
-/**
- * Classe di enumerazione AnnoCorso, usata per elencare( tradizionalmente in numeri romani) gli anni di corso universitari 
- */
-public enum AnnoCorso {
-I,
-II,
-III
-}
