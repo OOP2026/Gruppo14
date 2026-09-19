6ca5de1f29a075930e0ba4423df6a2c15336f4c0
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-09-19 17:55:14 +0200

    Update GestioneLezioniFrame.java

--- src/main/java/gui/GestioneLezioniFrame.java
+++ src/main/java/gui/GestioneLezioniFrame.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 public class GestioneLezioniFrame extends JFrame {
 
-    private controller controller;
+    private Controller controller;
     private JComboBox<Insegnamento> comboInsegnamento;
     private JComboBox<String> comboGiorno;
     private JTextField txtOraInizio;
