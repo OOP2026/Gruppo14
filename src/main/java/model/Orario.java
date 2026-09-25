3abc03606dfa463c9815504e1bcc54a4c12b8d36
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-09-25 19:20:57 +0200

    progetto

--- src/main/java/model/Orario.java
+++ src/main/java/model/Orario.java
@@ -2,46 +2,33 @@ package model;
 import java.util.*;
 import java.time.*;
 public class Orario {
-String idOrario;
 Insegnamento corso;
+String materia;
 AnnoCorso anno;
 GiornoSettimana giorno;
 LocalTime inizio;
 LocalTime fine;
 Docente docente;
 Aula aula;
-ArrayList<Lezione> lezioni;
+ArrayList<Lezione> l;
 /**
- * Costruttore della classe Orario:
- * @param corso
- * @param anno
- * @param l
- * @param g
- * @param i
- * @param f
- * @param d
- * @param a
+ * Costruttore della classe Orario
  */
-	public Orario(String idOrario,AnnoCorso anno) {
-		this.idOrario=idOrario;
-		this.anno=anno;
-		lezioni=new ArrayList<>();
-		// TODO Auto-generated constructor stub
-	}
+public Orario() {
+	this.l=new ArrayList<>();
+}
 	/**
       * Verifica i conflitti tra due o più lezioni nello stesso orario
 	 * @param altraLez
-	 * @return
+	 * @return true o false a seconda se sono presenti conflitti o meno
 	 */
 	public boolean verificaConflitti(Lezione altraLez) {
 		if (this.giorno !=altraLez.getGiorno()) {
             return false;
         }
-        
+     
         /**
          *  Verifica la sovrapposizione temporale (intervalli aperti/chiusi)
-         */
-        /**
          *  C'è sovrapposizione se l'inizio di una è prima della fine dell'altra e viceversa
          */
         boolean sovrapposizioneTemporale = this.inizio.isBefore(altraLez.getOraFine()) && this.fine.isAfter(altraLez.getOraInizio());
@@ -60,38 +47,26 @@ ArrayList<Lezione> lezioni;
 		return "Orario [corso=" + corso.getNomeIns() + ", anno=" + anno + "]";
 	}
 	/**
- * Aggiunge una lezione verificando che non ci siano sovrapposizioni d'orario.
- * @param nuovaLezione la lezione da aggiungere
- * @throws Exception se la lezione si sovrappone a una già esistente
- */
-public void aggiungiLezione(Lezione nuovaLezione) throws Exception {
-    if (nuovaLezione == null) {
-        throw new IllegalArgumentException("La lezione non può essere nulla.");
-    }
-
-    for (Lezione l : lezioni) {
-        // Controlla se le due lezioni sono nello stesso giorno
-        if (l.getGiorno().equals(nuovaLezione.getGiorno())) {
-            
-            // Verifica sovrapposizione temporale:
-            // (inizio1 < fine2) AND (inizio2 < fine1)
-            boolean sovrapposizione = nuovaLezione.getOraInizio().isBefore(l.getOraFine()) && 
-                                     l.getOraInizio().isBefore(nuovaLezione.getOraFine());
-
-            if (sovrapposizione) {
-                throw new Exception("Conflitto d'orario: esiste già una lezione il " 
-                        + nuovaLezione.getGiorno() + " tra le " 
-                        + l.getOraInizio() + " e le " + l.getOraFine());
-            }
-        }
-    }
-
-    // Se non ci sono sovrapposizioni, aggiunge la lezione
-    this.lezioni.add(nuovaLezione);
-}
-	public ArrayList<Lezione> getLezioni(){
-		return lezioni;
+	 * Aggiunge una nuova lezione all'orario
+	 * @param l
+	 */
+	public void aggiungiLezione(Lezione l) {
+		this.l.add(l);
 	}
-}
+	/**
+	 * Rimuove una lezione dall'orario
+	 * @param l
+	 */
+	public void rimuoviLezione(Lezione l) {
+		this.l.remove(l);
+	}
+	/**
+	 * Ottiene le lezioni contenute nell'orario
+	 * @return l
+	 */
+	public List<Lezione> getLezioni(){
+		return l;
+	}
+	}
 
 
