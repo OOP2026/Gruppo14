a066c7c5cfad63177205d830aff77734738769d4
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-09-25 18:20:14 +0200

    progetto

--- src/main/java/model/Aula.java
+++ src/main/java/model/Aula.java
@@ -1,45 +1,61 @@
 package model;
 public class Aula {
-
-	// Attributo dal Class Diagram
     private String nomeAula;
 
-    // Relazione OSPITA (molteplicità 0..1 con Lezione)
-    private Lezione lezioneOspitata; // Può essere null se l'aula è libera
+    /**
+     *  Relazione OSPITA (molteplicità 0..1 con Lezione): può essere null se l'aula è libera
+     *  */
+    private Lezione lezioneOspitata; 
 
-    // Costruttore
+   /**
+    * Costruttore classe Aula
+    * @param nomeAula
+    */
     public Aula(String nomeAula) {
         this.nomeAula = nomeAula;
         this.lezioneOspitata = null; // Di default l'aula non ospita alcuna lezione
     }
 
-    // Getter e Setter per nomeAula
+    /**
+     * Ottiene il nome dell'aula
+     * @return nomeAula
+     */
     public String getNomeAula() {
         return nomeAula;
     }
-
+    /**
+     * Imposta un nuovo nome all'oggetto di classe Aula
+     * @param nomeAula
+     */
     public void setNomeAula(String nomeAula) {
         this.nomeAula = nomeAula;
     }
 
-    // Gestione della relazione con Lezione (0..1)
+    /**
+     * Ottiene il nome della lezione tenuta in quell'aula,se presente
+     * @return
+     */
     public Lezione getLezioneOspitata() {
         return lezioneOspitata;
     }
-
+    /**
+     * Setta un nuovo nome all'oggetto di classe Lezione
+     * @param lezione
+     */
     public void setLezioneOspitata(Lezione lezione) {
         this.lezioneOspitata = lezione;
     }
 
-    // Metodo di utilità per verificare se l'aula è libera
+   /**
+    * Metodo di utilità per verificare se l'aula è libera
+    * @return true o false
+    */
     public boolean isLibera() {
-        if(this.lezioneOspitata==null) {
-        	return true;
-        }
-        else {
-        	return false;
-        }
+        return this.lezioneOspitata==null;
     }
+    /**
+     * Metodo toString  serve a convertire o rappresentare un oggetto in formato di testo (stringa). 
+     */
     @Override
    	public String toString() {
    		return "Aula [nomeAula=" + nomeAula + ", lezioneOspitata=" + lezioneOspitata + "]";
