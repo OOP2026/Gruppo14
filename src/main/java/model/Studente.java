d493ccabf52786f2dc073c547c6bfdb2b87ed8b4
Matteokind0570 <mat.gentile@studenti.unina.it>
2026-09-26 17:44:46 +0200

    progetto

--- src/main/java/model/Studente.java
+++ src/main/java/model/Studente.java
@@ -1,24 +1,27 @@
 package model;
-
 public class Studente extends Utente {
 String matricola;
 AnnoCorso annoDiCorso;
-String facoltà;
+String facolta;
 Orario o;
+
 /**
- * Costruttore della classe Studente:
- * @param matricola
- * @param annoDiCorso
- * @param facoltà
- */
-/**
- * Eredita anch'essa dalla classe Utente
- */
-	public Studente(String nome,String cognome,String email,String login,String password,String matricola,AnnoCorso annoDiCorso,String facoltà) {
+	 * Costruttore della classe Studente eredita dalla classe Utente
+	 * @param nome
+	 * @param cognome
+	 * @param email
+	 * @param login
+	 * @param password
+	 * @param matricola:univoca per ciascuno studente
+	 * @param annoDiCorso
+	 * @param facoltà
+	 */
+	@SuppressWarnings({"java:S107","all"})
+	public Studente(String nome,String cognome,String email,String login,String password,String matricola,AnnoCorso annoDiCorso,String facolta) {
 		super(nome,cognome,email,login,password);
 		this.matricola=matricola;
 		this.annoDiCorso=annoDiCorso;
-		this.facoltà=facoltà;
+		this.facolta=facolta;
 }
 	/**
 	 * Mostra all'utente l'orario con tutte le lezioni
@@ -28,7 +31,19 @@ Orario o;
 	public Orario visualizzaOrario() {
 		return o;
 	}
-	public void setOrario(Orario o){
-		this.o=o;
-}
+	/**
+	 * Imposta un orario alla lezione
+	 * @param orario
+	 */
+	public void setOrario(Orario orario) {
+        this.o = orario;
+    }
+	/**
+	 * Ottiene l'orario di appartenenza della lezione
+	 * @return o
+	 */
+    public Orario getOrario() {
+        return this.o;
+    }
+
 }
